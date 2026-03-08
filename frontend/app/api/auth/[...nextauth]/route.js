1166289578e72acfe1bd42887c8d7be670de81e4
nelfi18-blip <nelfi18@gmail.com>
2026-03-08 17:58:29 -0400

    Refactor NextAuth configuration and callbacks

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -8,41 +8,31 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-
   secret: process.env.NEXTAUTH_SECRET,
-
-  session: {
-    strategy: "jwt",
-  },
-
+  session: { strategy: "jwt" },
   pages: {
     signIn: "/login",
     error: "/login",
   },
-
   callbacks: {
+    async signIn() {
+      return true;
+    },
     async jwt({ token, account, profile }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-
+      if (account) token.accessToken = account.access_token;
       if (profile) {
         token.name = profile.name;
         token.email = profile.email;
         token.picture = profile.picture;
       }
-
       return token;
     },
-
     async session({ session, token }) {
       if (token) {
         session.user.name = token.name;
         session.user.email = token.email;
         session.user.image = token.picture;
-        session.accessToken = token.accessToken;
       }
-
       return session;
     },
   },
